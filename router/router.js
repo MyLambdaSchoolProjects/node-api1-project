0182d1e29c10b89fb79a966d078ccfd18a80f3d9
David Isakson <ikeman32@yahoo.com>
2020-03-09 14:58:08 -0700

    setting up schema

--- router/router.js
+++ router/router.js
@@ -1,2 +1,22 @@
-const users = require('./model');
+const router = require("express").Router();
 
+const users = require("./model");
+
+router.post("/", (res, req) => {
+  const userData = req.body;
+  if (!userData.name || !userData.bio) {
+    res
+      .status(400)
+      .json({ message: "Please provide name and bio for the user." });
+  } else {
+    users
+      .insert(userData)
+      .then(user => {
+        res.status(201).json(userData);
+      })
+      .catch(error => {
+        console.log("Error on POST User", error);
+        res.status(500).json({ errorMessage: "error adding User" });
+      });
+  }
+});
